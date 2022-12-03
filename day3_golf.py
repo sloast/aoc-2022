e2dc13a9257c3a778361f7a10a1e8b88d40d8b6f
sloast <adaadrobson@gmail.com>
2022-12-03 19:19:39 +0000

    update day3_golf

--- day3_golf.py
+++ day3_golf.py
@@ -2,4 +2,9 @@
 print(sum(ord(s)-(96,38)[s<'a']for s in[(a&b).pop()for a,b in map(lambda x:(set(x[:len(x)//2]),set(x[len(x)//2:])),open('3'))]))
 
 # part 2
-print(sum(ord(s)-(96,38)[s<'a']for s in[(a&b&c).pop()for a,b,c in zip(*(map(lambda x:set(x),open('3')),)*3)]))
+# by me
+print(sum(ord(s)-(96,38)[s<'a']for s in[(a&b&c).pop()for a,b,c in zip(*(map(lambda x:set(x[:-1]),open('3')),)*3)]))
+print(sum(ord(s)-(96,38)[s<'a']for s in((a&b&c-set('\n')).pop()for a,b,c in zip(*(map(set,open("3")),)*3))))
+
+# @geo#5319's solution
+j="".join;print(sum(ord(s)-58*(s>'Z')-24 for s in j(j(a&b&c)for a,b,c in zip(*(map(set,open("3")),)*3))))
