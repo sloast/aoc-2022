9d02089036a649da9dd392343051a414fe086de8
sloast <adaadrobson@gmail.com>
2022-12-05 10:49:08 +0000

    update day5

--- day5.py
+++ day5.py
@@ -4,26 +4,28 @@ a = (' ' + a.replace('\n', '\n ')).replace('    ', ' -')
 for c in list(zip(*[l.split() for l in a.split('\n')])):
     arr.append(list(reversed(list(map(lambda x:x[1], filter(lambda y:len(y)>2,c))))))
 
-#for l in b.split('\nm'):
-#    l = [*map(int,l.split(' ')[1::2])]
-#    for i in range(l[0]):
-#        if arr[l[1]-1]:
-#            arr[l[2]-1].append(arr[l[1]-1].pop())
+arr2=[a.copy() for a in arr]
+for l in b.split('\nm'):
+    l = [*map(int,l.split(' ')[1::2])]
+    for i in range(l[0]):
+        if arr[l[1]-1]:
+            arr[l[2]-1].append(arr[l[1]-1].pop())
 
 for l in b.split('\nm'):
     l = [*map(int,l.split(' ')[1::2])]
     tmp = []
     for i in range(l[0]):
-        if arr[l[1]-1]:
-            tmp.append(arr[l[1]-1].pop())
-    arr[l[2]-1].extend(reversed(tmp))
+        if arr2[l[1]-1]:
+            tmp.append(arr2[l[1]-1].pop())
+    arr2[l[2]-1].extend(reversed(tmp))
 
 outputarr = []
 left = True
-for i in range(max(map(len,arr))):
-    outputarr.append(' '.join(j[i] if len(j)>i else ' ' for j in arr))
+for i in range(max(map(len,arr2))):
+    outputarr.append(' '.join(j[i] if len(j)>i else ' ' for j in arr2))
 print('\n'.join(reversed(outputarr)))
-print(' '.join(str(i+1) for i in range(len(arr))))
+print(' '.join(str(i+1) for i in range(len(arr2))))
 print()
 
-print(''.join([a.pop() for a in arr]))
+print('Part 1:',''.join([a.pop() for a in arr]))
+print('Part 2:',''.join([a.pop() for a in arr2]))
